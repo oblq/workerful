99c2de7a74c977b405d72155533d2b8c6494955c
Marco Muratori <marcomrtr@gmail.com>
2018-07-02 16:20:38 +0200

    finished

--- workerful.go
+++ workerful.go
@@ -7,8 +7,20 @@ import (
 	"runtime"
 	"sync"
 
+	"go.uber.org/atomic"
 	"gopkg.in/yaml.v2"
 )
+
+// Job is a job interface, useful if you need to pass parameters or do more complicated stuff
+type Job interface {
+	F() error
+}
+
+// SimpleJob is a job func useful for simple operations
+type SimpleJob func() error
+
+// JobQueue is a job queue in which you can append jobs.
+type jobQueue chan interface{}
 
 // Config defines the config for workerful.
 type Config struct {
@@ -16,18 +28,20 @@ type Config struct {
 	Workers   int `yaml:"workers"`
 }
 
-// Job is a job interface, it only has to have an Execute() func
-type Job interface {
-	DoTheJob()
-}
-
-// JobQueue is a job queue in which you can append jobs.
-type jobQueue chan interface{}
-
 // Workerful is the workerful instance type.
 type Workerful struct {
-	Config   *Config
-	jobQueue jobQueue
+	Config       *Config
+	jobQueue     jobQueue
+	workersGroup *sync.WaitGroup
+
+	// closer wait until the number of routines opened by PushFuncAsync() or PushJobAsync(),
+	// to send jobs to the buffered jobQueue, when it is already full, goes to zero before to close the queue.
+	// Necessary to avoid sending jobs in the jobQueue when it has been closed.
+	stopGroup   *sync.WaitGroup
+	queueClosed bool
+
+	DoneCount   atomic.Int64
+	FailedCount atomic.Int64
 }
 
 var once sync.Once
@@ -74,48 +88,58 @@ func NewWorkerful(configPath string, config *Config) *Workerful {
 		config = &Config{0, 0}
 	}
 
-	if config.QueueSize == 0 {
-		config.QueueSize = 10000
-	}
-
 	if config.Workers == 0 {
 		config.Workers = runtime.NumCPU()
+		runtime.GOMAXPROCS(config.Workers)
 	}
 
 	wp := &Workerful{
-		Config:   config,
-		jobQueue: make(jobQueue, config.QueueSize),
+		Config:       config,
+		jobQueue:     make(jobQueue, config.QueueSize),
+		workersGroup: &sync.WaitGroup{},
+		queueClosed:  false,
+		stopGroup:    &sync.WaitGroup{},
 	}
 
 	// Create workers
-	wg := &sync.WaitGroup{}
-	wg.Add(config.Workers)
-	for i := 1; i <= config.Workers; i++ {
-		go newWorker(wp.jobQueue, wg)
+	wp.workersGroup.Add(wp.Config.Workers)
+	for i := 1; i <= wp.Config.Workers; i++ {
+		go wp.newWorker()
 	}
+
+	println("[workerful] started...")
 
 	// Wait for workers to complete
 	// Wait() blocks until the WaitGroup counter is zero and the channel closed
 	// we don't need it
 	go func() {
-		wg.Wait()
-		println("jobQueue closed...")
+		wp.workersGroup.Wait()
+		println("[workerful] gracefully stopped...")
 	}()
 
 	return wp
 }
 
-func newWorker(jq jobQueue, wg *sync.WaitGroup) {
-	defer wg.Done()
+func (wp *Workerful) newWorker() {
 	// range over channels only stop after the channel has been closed
 	// wg.Done() then is never called until jobQueue is closed: close(jobQueue)
 	// so we have 'maxWorkers' routines executing jobs in chunks forever
-	for job := range jq {
+	defer wp.workersGroup.Done()
+
+	for job := range wp.jobQueue {
 		switch job.(type) {
 		case Job:
-			job.(Job).DoTheJob()
-		case func():
-			job.(func())()
+			if err := job.(Job).F(); err != nil {
+				wp.FailedCount.Inc()
+			} else {
+				wp.DoneCount.Inc()
+			}
+		case SimpleJob:
+			if err := job.(SimpleJob)(); err != nil {
+				wp.FailedCount.Inc()
+			} else {
+				wp.DoneCount.Inc()
+			}
 		default:
 			log.Println("[workerful] Push() func only accept `Job` (see workerful.Job interface) and `func()` types")
 			continue
@@ -123,23 +147,102 @@ func newWorker(jq jobQueue, wg *sync.WaitGroup) {
 	}
 }
 
-// PushJob is an helper method that add a Job to the queue whithout blocking.
-// When the jobQueue is full wp.JobQueue <- job block the current routine until it finish the queued jobs
-func (wp Workerful) PushJob(job Job) {
-	go func() {
-		wp.jobQueue <- job
-	}()
-}
-
-// PushFunc is an helper method that add a func() to the queue whithout blocking.
-// When the jobQueue is full wp.JobQueue <- job block the current routine until it finish the queued jobs
-func (wp Workerful) PushFunc(job func()) {
-	go func() {
-		wp.jobQueue <- job
-	}()
-}
-
-// Close close the jobQueue
-func (wp Workerful) Close() {
+// Stop close the jobQueue, gracefully, it is blocking.
+// Already queued jobs will be processed.
+// It is possible to Stop and (re)Start Workerful at any time.
+// If you continue to send async funcs/jobs after Stop()
+// it will block until all of the queue elements has been processed.
+func (wp *Workerful) Stop() {
+	wp.stopGroup.Wait()
+	wp.queueClosed = true
 	close(wp.jobQueue)
+}
+
+// Restart will launch the workers to process jobs.
+// It is possible to Stop and (re)Start Workerful at any time.
+func (wp *Workerful) Restart() {
+	wp.jobQueue = make(jobQueue, wp.Config.QueueSize)
+
+	// Create workers
+	wp.workersGroup = &sync.WaitGroup{}
+	wp.workersGroup.Add(wp.Config.Workers)
+	for i := 1; i <= wp.Config.Workers; i++ {
+		go wp.newWorker()
+	}
+
+	// Create stopper
+	wp.stopGroup = &sync.WaitGroup{}
+	wp.queueClosed = false
+
+	println("[workerful] restarted...")
+
+	// Wait for workers to complete
+	// Wait() blocks until the WaitGroup counter is zero and the channel closed
+	// we don't need it
+	go func() {
+		wp.workersGroup.Wait()
+		println("[workerful] gracefully stopped...")
+	}()
+}
+
+// Status return the number of processed, failed and inQueue jobs.
+func (wp *Workerful) Status() (done int64, failed int64, inQueue int) {
+	return wp.DoneCount.Load(), wp.FailedCount.Load(), len(wp.jobQueue)
+}
+
+func (wp *Workerful) canPush() bool {
+	if wp.queueClosed {
+		wp.FailedCount.Inc()
+		println("[workerful] the queue is closed, can't push a new job")
+		return false
+	}
+	return true
+}
+
+// PushJob is an helper method that add a Job to the queue.
+// With a buffered chan queue (queue_size != 0),
+// when it is full wp.JobQueue <- job block the current routine until it free a space.
+func (wp *Workerful) PushJob(job Job) {
+	if !wp.canPush() {
+		return
+	}
+	wp.jobQueue <- job
+}
+
+// PushJobAsync is an helper method that add a Job to the queue whithout blocking.
+// With a buffered chan queue (queue_size != 0),
+// when it is full wp.JobQueue <- job does not block the current routine.
+func (wp *Workerful) PushJobAsync(job Job) {
+	if !wp.canPush() {
+		return
+	}
+	wp.stopGroup.Add(1)
+	go func() {
+		wp.jobQueue <- job
+		wp.stopGroup.Done()
+	}()
+}
+
+// PushFunc is an helper method that add a func() to the queue.
+// With a buffered chan queue (queue_size != 0),
+// when it is full wp.JobQueue <- job block the current routine until it free a space.
+func (wp *Workerful) PushFunc(job SimpleJob) {
+	if !wp.canPush() {
+		return
+	}
+	wp.jobQueue <- job
+}
+
+// PushFuncAsync is an helper method that add a func() to the queue whithout blocking.
+// With a buffered chan queue (queue_size != 0),
+// when it is full wp.JobQueue <- job does not block the current routine.
+func (wp *Workerful) PushFuncAsync(job SimpleJob) {
+	if !wp.canPush() {
+		return
+	}
+	wp.stopGroup.Add(1)
+	go func() {
+		wp.jobQueue <- job
+		wp.stopGroup.Done()
+	}()
 }
--- workerful_test.go
+++ workerful_test.go
@@ -11,23 +11,25 @@ type CustomJob struct {
 	ID        int
 }
 
-func (mj CustomJob) DoTheJob() {
+func (mj CustomJob) F() error {
 	time.Sleep(time.Second)
 	//println("job", mj.ID, "executed...")
 	mj.Responses <- 1
+	return nil
 }
 
-func TestJobs(t *testing.T) {
+func TestBufferedWUnblockingPush(t *testing.T) {
 	jobsNum := 48
+	queueSize := 16
 	workers := 16
-	wp := NewWorkerful("", &Config{jobsNum, workers})
+	wp := NewWorkerful("", &Config{queueSize, workers})
 
 	responses := make(chan int, jobsNum)
 	start := time.Now()
 
 	i := 0
 	for i < int(jobsNum) {
-		wp.PushJob(CustomJob{responses, i})
+		wp.PushJobAsync(CustomJob{responses, i})
 		i++
 	}
 
@@ -39,35 +41,35 @@ func TestJobs(t *testing.T) {
 		}
 	}
 
-	wp.Close()
+	wp.Stop()
 
-	timeElapsed := time.Since(start).Seconds()
-	timeNeeded := float64(jobsNum) / float64(workers)
+	timeElapsed := time.Since(start)
+	timeNeeded := time.Duration(float64(jobsNum)/float64(workers)) * time.Second
 
 	if timeElapsed < timeNeeded {
-		t.Errorf("Time elapsed (%f) too short (time needed: %f), something is wrong", timeElapsed, timeNeeded)
+		t.Errorf("Time elapsed (%f) too short (time needed: %f), something is wrong", timeElapsed.Seconds(), timeNeeded.Seconds())
 	}
 
-	if timeElapsed > (timeNeeded * 1.1) {
-		t.Errorf("Time elapsed (%f) too long (time needed * 1.1: %f), something is wrong", timeElapsed, timeNeeded*1.1)
+	if timeElapsed > (timeNeeded + time.Second) {
+		t.Errorf("Time elapsed (%f) too long than (time needed + 1 sec): %f, something is wrong", timeElapsed.Seconds(), (timeNeeded + time.Second).Seconds())
 	}
 }
 
-func TestFuncs(t *testing.T) {
+func TestBufferedWBlockingPush(t *testing.T) {
 	jobsNum := 48
+	queueSize := 16
 	workers := 16
-	wp := NewWorkerful("", &Config{jobsNum, workers})
+	wp := NewWorkerful("", &Config{queueSize, workers})
 
 	responses := make(chan int, jobsNum)
 	start := time.Now()
 
 	i := 0
 	for i < int(jobsNum) {
-		//j := i
-		wp.PushFunc(func() {
+		wp.PushFunc(func() error {
 			time.Sleep(time.Second)
-			//println("job", j, "executed...")
 			responses <- 1
+			return nil
 		})
 		i++
 	}
@@ -80,16 +82,114 @@ func TestFuncs(t *testing.T) {
 		}
 	}
 
-	wp.Close()
+	wp.Stop()
 
-	timeElapsed := time.Since(start).Seconds()
-	timeNeeded := float64(jobsNum) / float64(workers)
+	timeElapsed := time.Since(start)
+	timeNeeded := time.Duration(float64(jobsNum)/float64(workers)) * time.Second
 
 	if timeElapsed < timeNeeded {
-		t.Errorf("Time elapsed (%f) too short (time needed: %f), something is wrong", timeElapsed, timeNeeded)
+		t.Errorf("Time elapsed (%f) too short (time needed: %f), something is wrong", timeElapsed.Seconds(), timeNeeded.Seconds())
 	}
 
-	if timeElapsed > (timeNeeded * 1.1) {
-		t.Errorf("Time elapsed (%f) too long (time needed * 1.1: %f), something is wrong", timeElapsed, timeNeeded*1.1)
+	if timeElapsed > (timeNeeded + time.Second) {
+		t.Errorf("Time elapsed (%f) too long than (time needed + 1 sec): %f, something is wrong", timeElapsed.Seconds(), (timeNeeded + time.Second).Seconds())
 	}
+}
+
+func TestUnbufferedWBlockingPush(t *testing.T) {
+	jobsNum := 48
+	workers := 16
+	wp := NewWorkerful("", &Config{0, workers})
+
+	responses := make(chan int, jobsNum)
+	start := time.Now()
+
+	i := 0
+	for i < int(jobsNum) {
+		wp.PushFunc(func() error {
+			time.Sleep(time.Second)
+			responses <- 1
+			return nil
+		})
+		i++
+	}
+
+	count := 0
+	for i := range responses {
+		count += i
+		if count == jobsNum {
+			close(responses)
+		}
+	}
+
+	wp.Stop()
+
+	timeElapsed := time.Since(start)
+	timeNeeded := time.Duration(float64(jobsNum)/float64(workers)) * time.Second
+
+	if timeElapsed < timeNeeded {
+		t.Errorf("Time elapsed (%f) too short (time needed: %f), something is wrong", timeElapsed.Seconds(), timeNeeded.Seconds())
+	}
+
+	if timeElapsed > (timeNeeded + time.Second) {
+		t.Errorf("Time elapsed (%f) too long than (time needed + 1 sec): %f, something is wrong", timeElapsed.Seconds(), (timeNeeded + time.Second).Seconds())
+	}
+}
+
+func TestStopStart(t *testing.T) {
+	jobsNum := 192
+	workers := 48
+	wp := NewWorkerful("", &Config{jobsNum, workers})
+
+	var pause time.Duration = 5
+
+	start := time.Now()
+
+	responses := make(chan int, jobsNum)
+
+	i := 0
+	for i < int(jobsNum/2) {
+		wp.PushFuncAsync(func() error {
+			time.Sleep(time.Second)
+			responses <- 1
+			return nil
+		})
+		i++
+	}
+
+	wp.Stop()
+	time.Sleep(pause * time.Second)
+	wp.Restart()
+
+	i = 0
+	for i < int(jobsNum/2) {
+		wp.PushFuncAsync(func() error {
+			time.Sleep(time.Second)
+			responses <- 1
+			return nil
+		})
+		i++
+	}
+
+	wp.Stop()
+
+	count := 0
+	for i := range responses {
+		count += i
+		if count == jobsNum {
+			close(responses)
+		}
+	}
+
+	timeElapsed := time.Since(start)
+	timeNeeded := (time.Duration(float64(jobsNum)/float64(workers)) + pause) * time.Second
+
+	if timeElapsed < timeNeeded-2*time.Second {
+		t.Errorf("Time elapsed (%f) too short (time needed: %f), something is wrong", timeElapsed.Seconds(), timeNeeded.Seconds())
+	}
+
+	if timeElapsed > (timeNeeded - 1*time.Second) {
+		t.Errorf("Time elapsed (%f) too long than (time needed + 1 sec): %f, something is wrong", timeElapsed.Seconds(), timeNeeded.Seconds())
+	}
+
 }
